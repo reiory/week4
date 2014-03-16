fe91b98ce0114f0271df6cc84cf3966fdb9a366f
reiory <reiory820402@gmail.com>
2014-03-17 03:27:19 +0800

    Create Dealer.java

---
+++ Dealer.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Dealer extends GamePlayer{
+	public Dealer(){
+		super("Dealer");
+	}
+	/* public void dealGame(){
+		int opt=0;
+		while(super.countPint()<17){
+			Hit(draw());	
+		}
+		super.showCards();
+		boolean h = super.bustCheck();
+	} */
+}
