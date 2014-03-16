810c8261036e650fe31f8c48f49fa162cf003ba7
reiory <reiory820402@gmail.com>
2014-03-17 03:24:53 +0800

    Create Card.java

---
+++ Card.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+class Card{
+	private ArrayList<Card> cards = new ArrayList<Card>();
+	private String suitName;
+	private int cardNumber;
+	public Card(String suitNumber,int value){
+		this.suitName = suitNumber;
+		this.cardNumber = value;
+	}
+	public String suitRecall(){
+		return this.suitName;
+	}
+	public int cardRecall(){
+		return this.cardNumber;
+	}
+	public void Show(){
+		System.out.println(suitRecall() + " " + cardRecall());
+	}
+}
